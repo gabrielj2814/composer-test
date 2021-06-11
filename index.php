64fd66484f66521b64cc4cc63f47c051fd300484
gabriel valera <hashiramakage@gmail.com>
2021-06-11 15:46:54 -0400

    test de la api php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+$request = Request::createFromGlobals(); //acá creamos el objeto request
+
+if($request->isMethod("POST")){
+    // $responseJson= new JsonResponse();
+    print("POST");
+}
+else if($request->isMethod("GET")){
+    print("GET");
+    
+}
+
 ?>
